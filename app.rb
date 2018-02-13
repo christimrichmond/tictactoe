078a1bdbf26f253ddb728a7ede944ae5369ea819
christimrichmond <christimrichmond@gmail.com>
2018-02-13 15:04:04 -0500

    not much has changed...

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require "sinatra"
-enable :sessions
+require_relative "tictactoe.rb"
 
 get '/' do
   erb :home
 end
 
-# post 'name' do
-# 	session[:name] = params[:name]
-# 	redirect '/'
+# post '/input' do
+# 	@x = params[:x]
+# 	@o = params[:o]
+# 	redirect 'results?x=' + num1 + '&num2=' + num2 + '&num3=' + num3 + '&num4=' + num4 + '&num5=' + num5 + '&num6=' + num6
+# end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,14 +1,43 @@
-  
+    class Players
+    	def x_player
+    		@x = "X"
+    	end
+ 		
+ 		def o_player
+ 			@o = "O"
+ 		end
+ 	end
+
+# def turns
+# 	if 
+# end
+
+# def x_move
+# 	if "X" && a1 == true
+# 		puts X
+# 	else
+# 		puts O
+# end
+
+		
+
+# def x_win
+# 	if @x = x_winner 
+# end
+
+# def o_win
+
+# end
+
+
+
+# board = [[a1, a2, a3], [b4, b5, b6], [c7, c8, c9]]
+
+# x_winner = [a1 => X, a2, a3], [b4, b5, b6], [c7, c8, c9], [a1, b5, c9], [a3, b5, c7], [a1, b4, c7] [a2, b5, c8], [a3, b6, c9]
+
+# o_winner = [a1 => O, a2, a3], [b4, b5, b6], [c7, c8, c9], [a1, b5, c9], [a3, b5, c7], [a1, b4, c7] [a2, b5, c8], [a3, b6, c9]
 
 
 
 
-
-    # X_WIN_PATTERNS = [/XXX....../, /...XXX.../, /......XXX/,
-    #                   /X..X..X../, /.X..X..X./, /..X..X..X/,
-    #                   /X...X...X/, /..X.X.X../]
-             
-    # O_WIN_PATTERNS = [/OOO....../, /...OOO.../, /......OOO/,
-    #                   /O..O..O../, /.O..O..O./, /..O..O..O/,
-    #                   /O...O...O/, /..O.O.O../]
 
