22a1f6197875a3bc78cdc0ed1b5446dedc1ddca3
christimrichmond <christimrichmond@gmail.com>
2018-02-13 23:01:05 -0500

    more attempts, still very unfinished

--- app.rb
+++ app.rb
@@ -5,8 +5,3 @@ get '/' do
   erb :home
 end
 
-# post '/input' do
-# 	@x = params[:x]
-# 	@o = params[:o]
-# 	redirect 'results?x=' + num1 + '&num2=' + num2 + '&num3=' + num3 + '&num4=' + num4 + '&num5=' + num5 + '&num6=' + num6
-# end
--- tictactoe.rb
+++ tictactoe.rb
@@ -8,34 +8,33 @@
  		end
  	end
 
-# def turns
-# 	if 
-# end
+def moves
+	if @x && board == true
+			puts "X"
+	elsif
+		@o && board == true
+			puts "O"
+	else
+		false
+	end
+end
 
-# def x_move
-# 	if "X" && a1 == true
-# 		puts X
-# 	else
-# 		puts O
-# end
-
-		
-
-# def x_win
-# 	if @x = x_winner 
-# end
-
-# def o_win
-
-# end
-
+def winner
+	if @x = x_winner 
+		puts "X wins!"
+	elsif @o = o_winner
+		puts "O wins!"
+	else
+		puts "-Tie-"
+end
 
 
-# board = [[a1, a2, a3], [b4, b5, b6], [c7, c8, c9]]
 
-# x_winner = [a1 => X, a2, a3], [b4, b5, b6], [c7, c8, c9], [a1, b5, c9], [a3, b5, c7], [a1, b4, c7] [a2, b5, c8], [a3, b6, c9]
+board = [[a1, a2, a3], [b4, b5, b6], [c7, c8, c9]]
 
-# o_winner = [a1 => O, a2, a3], [b4, b5, b6], [c7, c8, c9], [a1, b5, c9], [a3, b5, c7], [a1, b4, c7] [a2, b5, c8], [a3, b6, c9]
+x_winner = [{"a1" => X, "a2" => X, "a3" => X}, {"b4" => X, "b5" => X, "b6" => X}, {"c7" => X, "c8" => X, "c9" => X}, {"a1" => X, "b5" => X, "c9" => X}, {"a3" => X, "b5" => X, "c7" => X}, {"a1" => X, "b4" => X, "c7" => X}, {"a2" => X, "b5" => X, "c8" => X}, {"a3" => X, "b6" => X, "c9" => X}]
+
+o_winner = [a1 => O, a2, a3], [b4, b5, b6], [c7, c8, c9], [a1, b5, c9], [a3, b5, c7], [a1, b4, c7] [a2, b5, c8], [a3, b6, c9]
 
 
 
